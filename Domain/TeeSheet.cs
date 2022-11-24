b01c801331acadba3b70f3ce256b9a1e58a8abff
Pavel Trninkov <ptrninkov1@nait.ca>
2022-11-24 13:44:09 -0700

    Add project files.

---
+++ Domain/TeeSheet.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BAIS3130.Domain
+{
+    public class TeeSheet
+    {
+    }
+}
---
+++ Technical Services/TeeTime.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BAIS3130.Technical_Services
+{
+    public class TeeTime
+    {
+    }
+}
